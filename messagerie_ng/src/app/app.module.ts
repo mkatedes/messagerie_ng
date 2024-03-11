d252818d4e01b0528120c68471f669f7c080cf83
unknown <matteo.geffroy@gmail.com>
2024-03-11 20:11:02 +0100

    display des elements

--- messagerie_ng/src/app/app.module.ts
+++ messagerie_ng/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MessagesComponent } from './messages/messages.component';
+import { InputComponent } from './input/input.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MessagesComponent,
+    InputComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
---
+++ messagerie_ng/src/app/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+  onSubmit() {
+    console.log();
+    // Do login here
+  }
+}
